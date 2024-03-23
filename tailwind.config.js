49dac75371442255cb91639e99e7a837001af700
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-03-23 09:19:28 +0530

    bar config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-  ],
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
 	mode: "jit",
 	theme: {
 		extend: {},
 	},
 	plugins: [],
 };
-
 
